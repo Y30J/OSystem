381092c1b562c3e6b6754adc3f5b920875e8174a
Y30J <1154621353@qq.com>
2022-11-08 11:07:23 +0800

    20301122-2.4

--- os/src/main.rs
+++ os/src/main.rs
@@ -82,3 +82,10 @@ pub fn rust_main() -> ! {
     loop{};
 }
 
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+}
