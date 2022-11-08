490f276f6d3c8bb0f66bd85a3c4d21f3463b6845
Y30J <1154621353@qq.com>
2022-11-08 11:43:32 +0800

    20301122-2.7

--- os/src/main.rs
+++ os/src/main.rs
@@ -3,40 +3,46 @@
 #![feature(panic_info_message)]
 #[macro_use]
 
-mod sbi;
 mod console;
 mod lang_items;
-
-pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
-  syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
-}
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
-use core::arch::asm;
-
-#[no_mangle]
-extern "C" fn _start() {
-    println!("Hello, world!");
-    sys_exit(9);
-}
+mod sbi;
 
 use core::arch::global_asm;
 
 global_asm!(include_str!("entry.asm"));
-
-#[no_mangle]
-pub fn rust_main() -> ! {
-    loop{};
-}
-
 fn clear_bss() {
     extern "C" {
         fn sbss();
         fn ebss();
     }
     (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+}
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    extern "C" {
+        fn stext();
+        fn etext();
+        fn srodata();
+        fn erodata();
+        fn sdata();
+        fn edata();
+        fn sbss();
+        fn ebss();
+        fn boot_stack();
+        fn boot_stack_top();
+    }
+    clear_bss();
+    println!("Hello, world!");
+    println!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
+    println!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
+    println!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
+    println!(
+        "boot_stack [{:#x}, {:#x})",
+        boot_stack as usize, boot_stack_top as usize
+    );
+    println!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+    println!("Hello, world!");
+    panic!("Shutdown machine!");
 }
 
