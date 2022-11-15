c83ab323b727b1e2cbc988ed9a272f69b0a8f134
Y30J <1154621353@qq.com>
2022-11-15 12:27:40 +0800

    7-20301122

--- os/src/trap/context.rs
+++ os/src/trap/context.rs
@@ -1,4 +1,12 @@
+
 use riscv::register::sstatus::{Sstatus, self, SPP};
+
+#[repr(C)]
+pub struct TrapContext {
+    pub x: [usize; 32],
+    pub sstatus: Sstatus,
+    pub sepc: usize,
+}
 
 impl TrapContext {
     pub fn set_sp(&mut self, sp: usize) { self.x[2] = sp; }
@@ -14,4 +22,3 @@ impl TrapContext {
         cx
     }
 }
-
