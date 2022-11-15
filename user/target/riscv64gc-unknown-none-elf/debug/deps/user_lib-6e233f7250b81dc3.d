68f17979b2ea298dcf1ab00242e73dde5c17e5b8
Y30J <1154621353@qq.com>
2022-11-15 11:48:45 +0800

    3.6+3.7-20301122

---
+++ user/target/riscv64gc-unknown-none-elf/debug/deps/user_lib-6e233f7250b81dc3.d
@@ -0,0 +1,5 @@
+/mnt/user/target/riscv64gc-unknown-none-elf/debug/deps/user_lib-6e233f7250b81dc3: src/main.rs
+
+/mnt/user/target/riscv64gc-unknown-none-elf/debug/deps/user_lib-6e233f7250b81dc3.d: src/main.rs
+
+src/main.rs:
